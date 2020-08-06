55b8b04bee5f7f413a1a78435696e5b581ee49a5
deepak <dk721572@gmail.com>
2020-08-06 19:14:31 +0530

    :octocat: init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    "react-app",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    module: "",
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    semi: false,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+  },
+};
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const index = () => (
+  <>
+    <h1>Home</h1>
+  </>
+);
+
+export default index;
