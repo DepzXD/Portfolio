fe4e2bea55d1c67ce886f23e7ea6201da3154ed6
deepak <dk721572@gmail.com>
2020-08-14 20:38:51 +0530

    👨‍🎓  contanct sec.

---
+++ src/components/Contact.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const BtnStyle = styled.button`
+  background: none;
+  color: ${({ theme }) => theme.secondary};
+  border: 3px solid ${({ theme }) => theme.secondary};
+  border-radius: 5px;
+  font-size: 1.2rem;
+  padding: 0.8rem;
+  font-weight: 600;
+  max-width: 10rem;
+`;
+
+const Contact = () => (
+  <section
+    css={css`
+      /* margin-left: 10%; */
+      display: flex;
+      flex-flow: column wrap;
+      align-content: center;
+      width: 80%;
+      align-items: center;
+
+      h1 {
+        font-size: 3.5rem;
+      }
+
+      p {
+        margin: 2rem 0;
+        width: 45%;
+      }
+    `}
+  >
+    <h1>Get into Touch</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequuntur
+      cumque sequi ipsum ipsa iste necessitatibus enim veritatis, reprehenderit
+      eius dolor ut facilis
+    </p>
+    <BtnStyle>Say Hi!</BtnStyle>
+  </section>
+);
+
+export default Contact;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
 import MyWorks from "../components/MyWorks";
+import Contact from "../components/Contact";
 
 const index = () => (
   <Layout>
     <Hero />
     <AboutMe />
     <MyWorks />
+    <Contact />
   </Layout>
 );
 
