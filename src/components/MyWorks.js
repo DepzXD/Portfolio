14e25354a42acd8f9512f25466d15ad76abdf6b7
deepak <dk721572@gmail.com>
2020-08-19 15:13:15 +0530

    cards updated

--- src/components/MyWorks.js
+++ src/components/MyWorks.js
@@ -1,17 +1,64 @@
 import React from "react";
 import { css } from "@emotion/core";
+import styled from "@emotion/styled";
 
 // Todo: About me h1 margin & skills margin
-// Todo: OverLap components
+
+const CardStyle = styled.div`
+  margin-top: 4rem;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: 1fr;
+  .ctn {
+    display: flex;
+    flex-flow: column;
+    border-radius: 0.5em;
+    height: 350px;
+    grid-column: 1 / 6;
+    grid-row: 1 / 1;
+    background: red;
+    padding: 0 1em;
+
+    h2 {
+      margin-top: 2rem;
+      font-size: 1.8rem;
+    }
+    p {
+      margin-top: 2rem;
+      width: 60%;
+    }
+
+    .links {
+      display: flex;
+      /* flex-flow: column; */
+
+      margin-top: 3rem;
+    }
+
+    .icons {
+      display: flex;
+      align-self: flex-end;
+      justify-self: flex-end;
+      margin-top: 3rem;
+    }
+  }
+  .preview {
+    border-radius: 0.5em;
+    background: blue;
+    grid-column: 5 / 8;
+    grid-row: 1 / 1;
+    margin: 1.5rem 0;
+    height: 250px;
+  }
+`;
+
 const MyWorks = () => {
   return (
     <section
       css={css`
         margin-left: 10%;
-
         display: flex;
         flex-flow: column wrap;
-        align-content: center;
         width: 80%;
 
         h1 {
@@ -20,38 +67,8 @@ const MyWorks = () => {
       `}
     >
       <h1>My Works</h1>
-      <div
-        css={css`
-          margin-top: 2rem;
-          width: 100%;
-          display: flex;
-          div {
-            border-radius: 5px;
-            padding: 2rem;
-          }
-          .xtn {
-            flex: 0 0 70%;
-            height: 20rem;
-            background: #131222;
-            display: flex;
-            flex-flow: column wrap;
-
-            justify-items: center;
-            p {
-              margin-top: 1rem;
-            }
-            a {
-              width: auto;
-            }
-          }
-          .xtn-image {
-            flex: 0 0 30%;
-            height: 20rem;
-            background: #130022;
-          }
-        `}
-      >
-        <div className="xtn">
+      <CardStyle>
+        <div className="ctn">
           <h2>Dududu do</h2>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod totam
@@ -59,13 +76,19 @@ const MyWorks = () => {
             dolore nemo ullam fugit architecto recusandae <br /> inventore odio
             officiis consectetur, nihil
           </p>
-          <>
+          <div className="links">
             <a href="#a">Live</a>
             <a href="#c">Code</a>
-          </>
+          </div>
+          <div className="icons">
+            <h3>asd</h3>
+            <h3>sad</h3>
+            <h3>sad</h3>
+            <h3>dsa</h3>
+          </div>
         </div>
-        <div className="xtn-image">X</div>
-      </div>
+        <div className="preview">X</div>
+      </CardStyle>
     </section>
   );
 };
