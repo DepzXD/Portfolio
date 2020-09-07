d5d47ddb8eaedcc3950268a816b7135a92c32e7f
deepak <dk721572@gmail.com>
2020-09-07 16:44:14 +0530

    🙅 404.

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const NoResult = () => (
+  <>
+    <h1>No page found</h1>
+    <Link to="/">go back</Link>
+  </>
+);
+
+export default NoResult;
