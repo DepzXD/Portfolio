0a77d7ff49aa700111320997b56b4e00fb307466
deepak <dk721572@gmail.com>
2020-09-15 18:55:44 +0530

    🖼  updated images

--- src/components/MyWorks.js
+++ src/components/MyWorks.js
@@ -3,16 +3,14 @@ import ContainerStyle from "./styles/ContainerStyle";
 import Portfolio from "./cards/Portfolio";
 import Werecat from "./cards/Werecat";
 import RudeWallet from "./cards/RudeWallet";
-import TicTacToe from "./cards/TicTacToe";
 import Bot from "./cards/Bot";
 const MyWorks = () => {
   return (
     <ContainerStyle id="Projects" className="auto">
-      <h1 id="o">My Works</h1>
+      <h1>My Works</h1>
       <Portfolio />
       <Bot />
       <Werecat />
-      <TicTacToe />
       <RudeWallet />
     </ContainerStyle>
   );
